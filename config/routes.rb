82e72260594abece151eaf89bbec85de71c1f291
Kwanele <kndenze@gmail.com>
2022-06-07 13:37:31 +0200

    skeptipay landing structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
+  get 'pages/blog', to: 'pages#blog'
 end
