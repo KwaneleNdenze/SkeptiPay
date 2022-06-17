a34d37f3a26e559c38d1e77ea84d316672105a23
Thato <thatosmk@gmail.com>
2022-06-17 12:24:22 +0200

    Production deploy (#4)
    * added nav active link helper
    
    * added helper spec
    
    * removed console tag in index page
    
    * changed active link helper spec to use 'context'
    
    * update README.md file
    
    * Adds devise, documentation updates, dashboard page (#3)
    
    * Heroku Deploy   (#5)
    
    * devise bug commit for review
    
    * implemented user authentication with devise
    
    * user model and sessions specs
    
    * added dashboard page for logged in users
    
    * added pg to production
    
    * bundler fix
    
    * added ruby to platforms
    
    * made changes to database.yml
    
    Co-authored-by: Kwanele <kndenze@gmail.com>
    Co-authored-by: Kwanele Ndenze <71318179+KwaneleNdenze@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'blog', to: 'pages#blog'
+  get 'dashboard', to: 'pages#dashboard'
 
 end
