3b672ce21be6325dc2e1d700599cc3a05e5578f6
Kwanele <kndenze@gmail.com>
2022-06-24 09:55:36 +0200

    withdrawal factory update

--- spec/factories/withdrawal.rb
+++ spec/factories/withdrawal.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :withdrawal do
-    account_id {Faker::Number.number(10)}
-    amount { Faker::Number.number(1000) }
+    account_id {Faker::Number.number(1)}
+    amount { Faker::Number.number(5) }
     verification_code { "123456" }
     status { Faker::Number.number(1) }   
   end
