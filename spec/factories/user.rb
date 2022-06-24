39683dffbbc2b7ba4b04a1a702165ee722abf86a
Kwanele <kndenze@gmail.com>
2022-06-24 12:37:19 +0200

    removed account id on user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.user_name }
     email { Faker::Internet.email }
-    phone_number '0987654321'
+    phone_number {'0987654321'}
     password {Faker::Internet.password}
-    role 'merchant'
-    account_id { Faker::Number.number(1) }
+    role {'merchant'}
   end
 end
