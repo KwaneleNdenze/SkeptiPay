9775a164fba923f97d73d3c3be4266ab39b28eb8
Kwanele <kndenze@gmail.com>
2022-07-04 14:22:49 +0200

    14:22 commit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
-
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
 module.exports = environment
+
